ff221303ba4241cf8b1ed5752920ff307f16ccfe
Alan Maldonado <business.alanm@gmail.com>
2019-11-20 12:52:11 -0600

    FIX

--- ColonyDetector/Source.cpp
+++ ColonyDetector/Source.cpp
@@ -31,30 +31,32 @@ Mat resizeImage(Mat src, float per) {
 Mat prepareImage(Mat src, bool filter, int filVal, int thresh, float globalRes) {
 	Mat temp = src.clone();
 	Mat originalResized;
+
 	/*Resize Image*/
 	temp = resizeImage(temp, globalRes);
 	originalResized = temp;
+
 	/*Convert to Grayscale*/
 	cvtColor(temp, temp, COLOR_BGR2GRAY);
+
 	/*Apply Threshold*/
 	temp = ApplyThreshold(temp);
+
 	/*Apply Canny*/
 	Canny(temp, temp, thresh, thresh *2);
+
 	/*Find Contours*/
 	vector<vector<Point>> contours;
-	vector<vector<Point>> filtered_contours;
-	vector<vector<Point>> final_contours;
 	vector<Vec4i> hierarchy;
-	vector<Vec4i> filtered_hierarchy;
-	vector<Vec4i> final_hierarchy;
-
 	findContours(temp, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
 	/*Filtering Contours by Area*/
+	vector<vector<Point>> filtered_contours;
+	vector<Vec4i> filtered_hierarchy;
 	for (int i = 0; i < contours.size(); i++) {
 		float area = contourArea(contours[i]);
 		if (filter) {
-			if (area > 10) {
+			if (area > filVal) {
 				filtered_contours.push_back(contours[i]);
 				filtered_hierarchy.push_back(hierarchy[i]);
 			}
@@ -78,6 +80,9 @@ Mat prepareImage(Mat src, bool filter, int filVal, int thresh, float globalRes) 
 	a rojo, hacia lo mismo que arriba si es que cumplia. (El codigo funciona, pero no nos da ninguna colonia correcta)*/
 
 	/*Calculate Contourn Centroid*/
+	/*
+	vector<vector<Point>> final_contours;
+	vector<Vec4i> final_hierarchy;
 	vector<Moments> mu(filtered_contours.size());
 	vector<Point> mc(filtered_contours.size());
 	for (int i = 0; i < filtered_contours.size(); i++) {
@@ -85,7 +90,6 @@ Mat prepareImage(Mat src, bool filter, int filVal, int thresh, float globalRes) 
 		mc[i] = Point(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
 	}
 
-	/*Analyze color in centroid*/
 	cvtColor(originalResized, originalResized, COLOR_BGR2HSV);
 	for (int i = 0; i < filtered_contours.size(); i++) {
 		Point center = mc[i];
@@ -99,16 +103,16 @@ Mat prepareImage(Mat src, bool filter, int filVal, int thresh, float globalRes) 
 			final_contours.push_back(filtered_contours[i]);
 			final_hierarchy.push_back(filtered_hierarchy[i]);
 		}
-	}
+	}*/
 
 	//HASTA AQUI TERMINA LA PARTE QUE FALTA
 
 	//Drawing Areas
 	Mat drawing = resizeImage(src, globalRes);
-	for (int i = 0; i < final_contours.size(); i++)
+	for (int i = 0; i < filtered_contours.size(); i++)
 	{
 		Scalar color = Scalar(0,255,0);
-		drawContours(drawing, final_contours, i, color, 2, 8, final_hierarchy, 0, Point());
+		drawContours(drawing, filtered_contours, i, color, 2, 8, filtered_hierarchy, 0, Point());
 	}
 
 	return drawing;
@@ -118,7 +122,7 @@ int main() {
 	/*Read Images*/
 	Mat src0 = imread("DSC_0112.JPG");
 	//prepImage(Map src, bool filter, int minArea, int cannyThreshold, float resizeVal)
-	Mat src = prepareImage(src0, true, 10, 120, .40);
+	Mat src = prepareImage(src0, true, 10, 120, 0.40);
 	show(resizeImage(src, .70), "Colonies");
 	cout << "Number of Colonies found: " << col_counter << endl;
 
